12b17428c6ac4f8763a004cda671e6e515849eae
Aman Kumar Singh <aman20012004@gmail.com>
2024-01-22 03:38:57 +0530

    Remove files from tracking

--- src/components/Functions.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export function journeyPage() {
-  window.location.href = "gallery.html";
-}
-export function heartPage() {
-  window.location.href = "heart.html";
-}
--- src/components/Image.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-function Image(props) {
-  return <img className={props.className} src={props.src} alt={props.alt} />;
-}
-export default Image;

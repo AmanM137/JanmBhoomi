e029f2858e0d78cd74ad0746e700c2f7bbe5d1f1
Aman Kumar Singh <aman20012004@gmail.com>
2024-01-22 03:57:20 +0530

    Remove files from tracking

--- src/components/Image.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-function Image(props) {
-  return <img className={props.className} src={process.env.PUBLIC_URL + '' + props.src} alt={props.alt} />;
-}
-export default Image;
